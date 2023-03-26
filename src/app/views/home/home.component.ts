debbf741d3e2fc3c7dd3a7ff92d0f5c9281b73eb
ebriopes <antonio.vargasrosales@gmail.com>
2023-03-26 01:21:12 -0600

    :sparkles: Feature add Home component

---
+++ src/app/views/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { NavbarService } from 'src/app/services/navbar.service';
+import { ACTIVITIES } from './home.constants';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent implements OnInit {
+  activities = ACTIVITIES;
+  user = 'User X';
+  title = 'HOME';
+
+  constructor(private readonly navService: NavbarService) {}
+
+  ngOnInit(): void {
+    this.navService.setTitle(this.title);
+  }
+}
---
+++ src/app/views/home/home.constants.ts
@@ -0,0 +1,31 @@
+export const ACTIVITIES = [
+  {
+    title: 'Citas',
+    content: 'Explora las citas de diferentes shows y personajes.',
+    subcontent: {
+      subTitle: 'Rick & Morty:',
+      subText:
+        'Conoce la información hasta el momento de los personajes en la serie',
+    },
+    redirectTo: '/quotes',
+    pathLabel: '',
+  },
+  {
+    title: 'Reservas',
+    content: '¿Necesitas apartar un lugar?',
+    subcontent: {
+      subText: 'Entra y haz tu reserva en un par de minutos',
+    },
+    redirectTo: '/booking',
+    pathLabel: '',
+  },
+  {
+    title: 'Acortador de links',
+    content: 'Compartir links largos puede ser toda una lata.',
+    subcontent: {
+      subText: 'Por eso te facilitamos el trabajo, entra, pega y copia',
+    },
+    redirectTo: '/shorter',
+    pathLabel: '',
+  },
+];
