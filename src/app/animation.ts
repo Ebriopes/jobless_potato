ffb8635c48c9f0d42344f3ed5ed0cfa4523f87a9
ebriopes <antonio.vargasrosales@gmail.com>
2023-03-26 17:58:33 -0600

    :dizzy: Fix: animation when is change  between views

--- src/app/animation.ts
+++ src/app/animation.ts
@@ -22,8 +22,8 @@ export const slideInAnimation = trigger('routeAnimations', [
     query(':enter', [style({ right: '-100%' })], { optional: true }),
     query(':leave', animateChild()),
     group([
-      query(':leave', [animate('300ms ease-out', style({ right: '100%' }))]),
-      query(':enter', [animate('300ms ease-out', style({ right: '0%' }))], {
+      query(':leave', [animate('600ms ease-out', style({ right: '100%' }))]),
+      query(':enter', [animate('600ms ease-out', style({ right: '0%' }))], {
         optional: true,
       }),
     ]),
@@ -47,40 +47,41 @@ export const slideInAnimation = trigger('routeAnimations', [
     group([
       query(
         ':leave',
-        [animate('200ms ease-out', style({ left: '100%', opacity: 0 }))],
+        [animate('500ms ease-out', style({ left: '100%', opacity: 0 }))],
         { optional: true },
       ),
-      query(':enter', [animate('300ms ease-out', style({ left: '0%' }))], {
+      query(':enter', [animate('600ms ease-out', style({ left: '0%' }))], {
         optional: true,
       }),
     ]),
   ]),
-  // transition('* <=> *', [
-  //   style({ position: 'relative' }),
-  //   query(
-  //     ':enter, :leave',
-  //     [
-  //       style({
-  //         position: 'absolute',
-  //         top: 0,
-  //         left: 0,
-  //         width: '100%',
-  //       }),
-  //     ],
-  //     { optional: true },
-  //   ),
-  //   query(':enter', [style({ left: '-100%' })], { optional: true }),
-  //   query(':leave', animateChild(), { optional: true }),
-  //   group([
-  //     query(
-  //       ':leave',
-  //       [animate('200ms ease-out', style({ left: '100%', opacity: 0 }))],
-  //       { optional: true },
-  //     ),
-  //     query(':enter', [animate('300ms ease-out', style({ left: '0%' }))], {
-  //       optional: true,
-  //     }),
-  //     query('@*', animateChild(), { optional: true }),
-  //   ]),
-  // ]),
+  transition('* <=> *', [
+    style({ position: 'relative', overflowY: 'hidden' }),
+    query(
+      ':enter, :leave',
+      [
+        style({
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          height: '100%',
+        }),
+      ],
+      { optional: true },
+    ),
+    query(':enter', [style({ top: '-100%' })], { optional: true }),
+    query(':leave', animateChild(), { optional: true }),
+    group([
+      query(
+        ':leave',
+        [animate('800ms ease-out', style({ top: '100%', opacity: 0 }))],
+        { optional: true },
+      ),
+      query(':enter', [animate('800ms ease-out', style({ top: '0%' }))], {
+        optional: true,
+      }),
+      query('@*', animateChild(), { optional: true }),
+    ]),
+  ]),
 ]);
