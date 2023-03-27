b58088cce210720938150477eeb16c22f6eaf9c1
ebriopes <antonio.vargasrosales@gmail.com>
2023-03-27 17:46:18 -0600

    :lock: Feat: add guard to authenticate the user access

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { authGuard } from './auth/auth.guard';
 
 const routes: Routes = [
-  { redirectTo: 'home', path: '', pathMatch: 'full' },
   {
     path: 'home',
     loadChildren: () =>
       import('./views/home/home.module').then((m) => m.HomeModule),
+    canActivate: [authGuard],
     data: {
       animation: 'HomePage',
     },
@@ -17,19 +18,28 @@ const routes: Routes = [
       import('./views/rick-and-morty/rick-and-morty.module').then(
         (m) => m.RickAndMortyModule,
       ),
+    canActivate: [authGuard],
     data: { animation: 'RickPage' },
   },
   {
     path: 'booking',
     loadChildren: () =>
       import('./views/booking/booking.module').then((m) => m.BookingModule),
+    canActivate: [authGuard],
     data: { animation: 'booking' },
   },
   {
     path: 'shorter',
     loadChildren: () =>
       import('./views/shorter/shorter.module').then((m) => m.ShorterModule),
+    canActivate: [authGuard],
     data: { animation: 'shorter' },
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./views/login/login.module').then((m) => m.LoginModule),
+    data: { animation: 'login' },
   },
   {
     path: '**',
